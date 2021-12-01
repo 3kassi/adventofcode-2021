c12e0bbcba83bcab2f3dbc3c980bb2338badc361
3kassi <95373116+3kassi@users.noreply.github.com>
2021-12-02 00:41:40 +0200

    part 2

--- day1.cpp
+++ day1.cpp
@@ -29,25 +29,50 @@ vector<int> getDepthsFromFile()
 
 }
 
+
 int countIncreases(vector<int> depths)
 {
   int count = 0;
-  for (int i = 1; i < depths.size(); i++)
-    if (depths[i] > depths[i-1]){
-    //  cout << i << " " << depths[i-1] << " " << depths[i] <<endl;
-      count++;
-    }
   
+  for (int i = 1; i < depths.size(); i++)
+  {
+    if (depths[i] > depths[i-1]){count++;}
+  }
+  
+  return count;
+}
+
+
+int getLimitForDepthWindows(int size){
+  int limit;
+  limit = size - 3;
+  return limit;
+}
+
+
+int threeDepthsIncreases(vector<int> depths)
+{
+  int count = 0;
+  int limit = getLimitForDepthWindows(depths.size());
+  for (int i = 0; i < limit; i++)
+  {
+    int sumA = depths[i] + depths[i+1] + depths[i+2];
+    int sumB = depths[i+1] + depths[i+2] + depths[i+3];
+    if (sumB > sumA){count++;}
+  }
+
     return count;
 }
+
 
 int main()
 {
   vector<int> depths;
   int count;
   depths = getDepthsFromFile();
-  count = countIncreases(depths);
+  //count = countIncreases(depths);
+  count = threeDepthsIncreases(depths);
   cout << "Measurement increases " << count << " times.";
-
+ 
 }
 
